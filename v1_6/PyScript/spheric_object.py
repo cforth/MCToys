9daf0dd73984413d391663b791bba1d2f6d0a146
chaifei <cforth@163.com>
2020-08-26 20:58:40 +0800

    生成球体的正确代码

--- v1_6/PyScript/spheric_object.py
+++ v1_6/PyScript/spheric_object.py
@@ -3,23 +3,22 @@ import csv
 
 
 def spheric_object_coordinate(a, b, z, r):
-    start_z = z - r
     spheric_object_coordinate_list = []
-    for r_index in range(0, r):
-        cl = circle_surface.circle_surface_coordinate(a, b, r_index)
+    for r_index in range(0, r+1):
+        cl = circle_surface.circle_surface_coordinate(a, b, int(round((r**2 - r_index**2)**0.5)))
         for cl_i in cl:
-            spheric_object_coordinate_list.append(cl_i + [start_z+r_index])
+            spheric_object_coordinate_list.append(cl_i + [z+r_index])
 
     for r_index in range(r, -1, -1):
-        cl = circle_surface.circle_surface_coordinate(a, b, r_index)
+        cl = circle_surface.circle_surface_coordinate(a, b, int(round((r**2 - r_index**2)**0.5)))
         for cl_i in cl:
-            spheric_object_coordinate_list.append(cl_i + [start_z+2*r-r_index])
+            spheric_object_coordinate_list.append(cl_i + [z-r_index])
 
     return spheric_object_coordinate_list
 
 
 if __name__ == '__main__':
-    cl = spheric_object_coordinate(0, 0, 0, 10)
+    cl = spheric_object_coordinate(0, 0, 0, 40)
     path = './spheric_object.csv'
 
     with open(path, 'w', newline="") as f:
